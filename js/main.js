d979967fe228fa873a534a039e0f674e9a915a26
Luis Henrique <lhkamma@gmail.com>
2023-05-09 18:37:45 -0300

    escopo finish

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+const messageIntro = (elemento) => {
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra;
+        }, 75 * i)
+    })
+}
+
+const titulo = document.querySelector('.digitando');
+messageIntro(titulo);
