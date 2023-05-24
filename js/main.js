16fdd1aeacef33f8d6d3aa1f7e8fe2fed9a01819
Luis Henrique <lhkamma@gmail.com>
2023-05-23 22:34:47 -0300

    intro-js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-// document.getElementById('scroll-link').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     const targetSection = document.getElementById('#about-me');
-//     const targetOffsetTop = targetSection.offsetTop;
+// BUTTON SCROLL
+window.onscroll = function() {
+    verificarPosicaoScroll();
+  };
   
-//     window.scrollTo({
-//       top: targetOffsetTop,
-//       behavior: 'smooth'
-//     });
-//   });
+  function verificarPosicaoScroll() {
+    var btnVoltarTopo = document.getElementById("btn-voltar-topo");
+    
+    if (document.documentElement.scrollTop > 200) {
+      btnVoltarTopo.style.display = "block";
+    } else {
+      btnVoltarTopo.style.display = "none";
+    }
+  }
+  
+  function voltarAoTopo() {
+    const posicaoAtual = document.documentElement.scrollTop;
+    if (posicaoAtual > 0) {
+      window.requestAnimationFrame(voltarAoTopo);
+      window.scrollTo(0, posicaoAtual - posicaoAtual / 10);
+    }
+  }
+  
   
